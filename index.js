7063e82aa82241affd59a53c6786fb97345e7e83
Filip Hnízdo <filip@bluejumpers.com>
2017-05-18 23:14:34 +0100

    Start

---
+++ index.js
@@ -0,0 +1,9 @@
+var config = {
+  port: 3000,
+  foursquareKey: "GLVSRGETGQAOFL01NNFNPPIG3RUFRIBCKWPXJSMSYUVD15V2",
+  foursquareSecret: "LXUN1X2B4DRF2EKMURYEZRPYBLML2MND5ZKOKWILD5KSZ2EE",
+  openWeatherKey: "4e36efb3cd43a9037a66de5e5d054493",
+  static: __dirname + "/home",
+};
+
+var rf = require("relativefiction")(config);
---
+++ readme.md
@@ -0,0 +1,8 @@
+Relative Fiction Library
+------------------------
+
+Home of https://relativefiction.com
+
+For the repository of the actual relative fiction software visit https://github.com/FilipNest/relativefiction
+
+This is for the library to host stories. i.e the Relative Fiction homepage.
